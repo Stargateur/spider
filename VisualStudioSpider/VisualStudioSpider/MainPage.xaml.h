5a4720662a9a25ffe41e3444c4282e711b729a51
costa_b <kevin1.costa@epitech.eu>
2015-10-20 14:38:32 +0400

    Add visualstudio

---
+++ VisualStudioSpider/VisualStudioSpider/MainPage.xaml.h
@@ -0,0 +1,21 @@
+﻿//
+// MainPage.xaml.h
+// Déclaration de la classe MainPage.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace VisualStudioSpider
+{
+	/// <summary>
+	/// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	};
+}
