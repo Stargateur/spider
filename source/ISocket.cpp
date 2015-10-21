826a5b5d7c87a842aabace0e0832a74938c5aade
Bertrand-Rapello Baptiste <baptiste.bertrand-rapello@epitech.eu>
2015-10-21 23:58:11 +0200

    ajout des fichiers ISocket et Socket

---
+++ source/ISocket.cpp
@@ -0,0 +1,16 @@
+//
+// ISocket.cpp for  in /home/bbr2394/renduTek3/cpp_spider/source
+// 
+// Made by Bertrand-Rapello Baptiste
+// Login   <bertra_l@epitech.net>
+// 
+// Started on  Wed Oct 21 22:30:57 2015 Bertrand-Rapello Baptiste
+// Last update Wed Oct 21 22:31:45 2015 Bertrand-Rapello Baptiste
+//
+
+#include "ISocket.hpp"
+
+ISocket::~ISocket()
+{
+  
+}
---
+++ source/Socket.cpp
@@ -0,0 +1,62 @@
+//
+// Socket.cpp for  in /home/bbr2394/renduTek3/cpp_spider/source
+// 
+// Made by Bertrand-Rapello Baptiste
+// Login   <bertra_l@epitech.net>
+// 
+// Started on  Wed Oct 21 22:51:48 2015 Bertrand-Rapello Baptiste
+// Last update Wed Oct 21 23:33:14 2015 Bertrand-Rapello Baptiste
+//
+
+#include "ISocket.hpp"
+
+Socket::Socket()
+{
+  
+}
+
+Socket::~Socket()
+{
+
+}
+
+bool Socket::server(std::string host, std::string port)
+{
+
+}
+
+bool Socket::client(std::string host, std::string port)
+{
+
+}
+
+bool Socket::accept(ISocket const & sock)
+{
+
+}
+
+std::string const & get_ip()
+{
+
+}
+
+bool Socket::standard_io(ISocket::io )
+{
+  
+}
+
+bool Socket::can_read()
+{
+  
+}
+
+bool Socket::can_write()
+{
+
+}
+
+ITime const & Socket::get_timeout()
+{
+  
+}
+
