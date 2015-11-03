eb9bfc097f1da2e04a76cf0073d7793949f2bbda
costa_b <kevin1.costa@epitech.eu>
2015-11-03 15:10:57 +0500

    windows_socket

---
+++ lib/Socket_Windows/Socket_Windows/ISocket.cpp
@@ -0,0 +1,15 @@
+//
+// ISocket.cpp for  in /home/bbr2394/renduTek3/cpp_spider/source
+// 
+// Made by Bertrand-Rapello Baptiste
+// Login   <bertra_l@epitech.net>
+// 
+// Started on  Wed Oct 21 22:30:57 2015 Bertrand-Rapello Baptiste
+// Last update Thu Oct 22 13:55:10 2015 Antoine Plaskowski
+//
+
+#include	"ISocket.hpp"
+
+ISocket::~ISocket()
+{  
+}
---
+++ lib/Socket_Windows/Socket_Windows/Socket_Windows.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#pragma comment(lib,"ws2_32.lib")
+
+#include "ISocket.hpp"
+#include "ITime.hpp"
+#include <ws2tcpip.h>
+#include <winsock2.h>
+
+class SocketWindows : public ISocket
+{
+public:
+	SocketWindows(std::string const &ip, SOCKET sock);
+	~SocketWindows(void);
+	ISocket	&accept(void) const;
+	static bool	select(ITime const *timeout = nullptr);
+	std::string const	&get_ip(void) const;
+	bool	can_read(void) const;
+	bool	want_read(void) const;
+	bool	can_write(void) const;
+	bool	want_write(void) const;
+	bool	want_read_write(void) const;
+	uintmax_t	read(uint8_t &buffer, uintmax_t size) const;
+	uintmax_t	write(uint8_t const &buffer, uintmax_t size) const;
+private:
+	SOCKET m_sock;
+	WSAEVENT m_event;
+	std::string m_ip;
+	static WSAData	m_wsadata;
+	static WSAEVENT	m_events[WSA_MAXIMUM_WAIT_EVENTS];
+	static DWORD	m_idx_events;
+};
